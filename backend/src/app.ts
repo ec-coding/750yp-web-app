6069f8459541256817ceef6ec0c59480521c8a2d
NTumminaro <nicholastumminaro@gmail.com>
2024-02-10 00:02:09 -0600

    railway testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -42,10 +42,10 @@ function getBaseUrl(){
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.AUTH0_SECRET,
   baseURL: getBaseUrl(),
-  clientID: process.env.AUTH_ID,
-  issuerBaseURL: process.env.AUTH_URL
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
 };
 
 //Cloudinary duh
