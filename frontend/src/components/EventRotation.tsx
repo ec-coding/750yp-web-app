f97ecf3d3efc7fed119f7ec866bbe7ab8f0561c7
Edwin Choi <edwin.choi.wdev@gmail.com>
2024-08-14 18:49:16 -0500

    add yp olympics event

--- frontend/src/components/EventRotation.tsx
+++ frontend/src/components/EventRotation.tsx
@@ -14,85 +14,9 @@ const EventRotation = () => {
     return (
         <>
             <Grid container justifyContent="center">
-                {/* <Grid
-                    xs={12}
-                    sm={6}
-                >
-                    <Paper
-                        sx={{
-                            width: { xs: '100%', sm: '100%', md: '500px' },
-                            boxShadow: '2px 4px 8px black',
-                            top: '10px',
-                            marginX: 'auto',
-                        }}
-                    >
-                        <Img src="https://i.imgur.com/ss7UO4V.png" />
-                    </Paper>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            textTransform: 'none',
-                            color: 'black',
-                            fontWeight: 'bold',
-                            fontSize: '20px',
-                            borderRadius: '60px',
-                            paddingX: 8,
-                            marginTop: 3,
-                            marginBottom: 10,
-                            bgcolor: '#f3ce49',
-                            boxShadow: '2px 4px 6px black',
-                        }}
-                    >
-                        <Link href="https://business.lewisvillechamber.org/events/details/soar-serve-750yp-30169?calendarMonth=2024-05-01"
-                            sx={{
-                                textDecoration: 'none',
-                                color: 'black',
-                            }}>
-                            Register Here
-                        </Link>
-                    </Button>
-                </Grid>
                 <Grid
                     xs={12}
-                    sm={6}
-                >
-                    <Paper
-                        sx={{
-                            width: { xs: '100%', sm: '100%', md: '500px' },
-                            boxShadow: '2px 4px 8px black',
-                            marginX: 'auto'
-                        }}
-                    >
-                        <Img src="https://i.imgur.com/LS4RVr7.png" />
-                    </Paper>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            textTransform: 'none',
-                            color: 'black',
-                            fontWeight: 'bold',
-                            fontSize: '20px',
-                            borderRadius: '60px',
-                            paddingX: 8,
-                            marginY: 3,
-                            bgcolor: '#f3ce49',
-                            boxShadow: '2px 4px 6px black',
-                        }}
-                    >
-                        <Link href="https://business.lewisvillechamber.org/events/details/750yp-rise-thrive-05-14-2024-29922?calendarMonth=2024-05-01"
-                            sx={{
-                                textDecoration: 'none',
-                                color: 'black',
-                            }}>
-                            Register Here
-                        </Link>
-                    </Button>
-                </Grid> */}
-                <Grid
-                    xs={12}
-                    sm={6}
+                    sm={12}
                 >
                     <Paper
                         sx={{
@@ -129,6 +53,59 @@ const EventRotation = () => {
                         </Link>
                     </Button>
                 </Grid>
+                <Grid xs={12} container>
+                    <Grid
+                        xs={12}
+                    >
+                        <Paper
+                            sx={{
+                                width: { xs: '100%', sm: '100%', md: '500px' },
+                                boxShadow: '2px 4px 8px black',
+                                top: '10px',
+                                marginX: 'auto',
+                            }}
+                        >
+                            <Img src="https://i.imgur.com/P4cW5yE.png" />
+                        </Paper>
+                    </Grid>
+                    <Grid
+                        xs={12}
+                    >
+                        <Paper
+                            sx={{
+                                width: { xs: '100%', sm: '100%', md: '500px' },
+                                boxShadow: '2px 4px 8px black',
+                                marginX: 'auto'
+                            }}
+                        >
+                            <Img src="https://i.imgur.com/sGtN577.png" />
+                        </Paper>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            sx={{
+                                textTransform: 'none',
+                                color: 'black',
+                                fontWeight: 'bold',
+                                fontSize: '20px',
+                                borderRadius: '60px',
+                                paddingX: 8,
+                                marginY: 3,
+                                marginBottom: 8,
+                                bgcolor: '#f3ce49',
+                                boxShadow: '2px 4px 6px black',
+                            }}
+                        >
+                            <Link href="https://business.lewisvillechamber.org/events/details/750yp-olympics-29936"
+                                sx={{
+                                    textDecoration: 'none',
+                                    color: 'black',
+                                }}>
+                                Register Here
+                            </Link>
+                        </Button>
+                    </Grid>
+                </Grid>
             </Grid>
             <Grid marginBottom={4}>
                 <Typography fontWeight="bold" variant="h6">
@@ -140,7 +117,7 @@ const EventRotation = () => {
             </Grid>
             <Grid marginBottom={4}>
                 <Typography variant="h6">
-                And lastly, be sure to check out our <a href="https://lp.constantcontactpages.com/sl/yrCKui6/750YPNewsletter">Monthly Newsletter!</a>
+                    And lastly, be sure to check out our <a href="https://lp.constantcontactpages.com/sl/yrCKui6/750YPNewsletter">Monthly Newsletter!</a>
                 </Typography>
             </Grid>
         </>
